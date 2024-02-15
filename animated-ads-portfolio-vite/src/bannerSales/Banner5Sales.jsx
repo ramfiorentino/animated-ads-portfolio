b99c732b774ae46dff1ea70ea9eacf5d201f2f7e
itafiorentino <mariabf.bcn@gmail.com>
2024-02-15 15:54:47 +0100

    sales banner 5 ready

--- animated-ads-portfolio-vite/src/bannerSales/Banner5Sales.jsx
+++ animated-ads-portfolio-vite/src/bannerSales/Banner5Sales.jsx
@@ -9,28 +9,25 @@ function Banner5Sales() {
       <>
   <div className={styles.banner}>
     <div id={styles.bgcontainer}>
-      <div className={styles.bggraphiccontainer}></div>
       <div className={styles.backgroundimages}>
         <img id={styles.bgimage} src={banner5} />
       </div>
       <div className={styles.textcontent}>
         <div className={styles.textcontent1}>
+        <p id={styles.upTo} className={styles.regulartext}>
+            Up to 50%<br/>OFF
+          </p>
           <h1 id={styles.super} className={styles.extraboldtext}>
             SUPER
           </h1>
           <div id={styles.sale} className={styles.extraboldtext}>
             SALE
           </div>
+          <p id={styles.specialoffer} className={styles.boldtext}>
+            SPECIAL OFFER!
+          </p>
           <p id={styles.weekend} className={styles.boldtext}>
             THIS WEEKEND ONLY
-          </p>
-          <p id={styles.upTo} className={styles.regulartext}>
-            Up to 50% OFF
-          </p>
-        </div>
-        <div className={styles.textcontent2}>
-        <p id={styles.specialoffer} className={styles.boldtext}>
-            SPECIAL <br/> OFFER!
           </p>
         </div>
       </div>
