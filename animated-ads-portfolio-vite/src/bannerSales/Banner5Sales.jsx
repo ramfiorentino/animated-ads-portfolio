dbbc7d44c3982a403a2802958aa32ba156a9eeec
itafiorentino <mariabf.bcn@gmail.com>
2024-02-15 15:13:23 +0100

    implementing css modules wip

--- animated-ads-portfolio-vite/src/bannerSales/Banner5Sales.jsx
+++ animated-ads-portfolio-vite/src/bannerSales/Banner5Sales.jsx
@@ -8,7 +8,33 @@ function Banner5Sales() {
     return (
       <>
   <div className={styles.banner}>
-   <img src={banner5} />
+    <div id={styles.bgcontainer}>
+      <div className={styles.bggraphiccontainer}></div>
+      <div className={styles.backgroundimages}>
+        <img id={styles.bgimage} src={banner5} />
+      </div>
+      <div className={styles.textcontent}>
+        <div className={styles.textcontent1}>
+          <h1 id={styles.super} className={styles.extraboldtext}>
+            SUPER
+          </h1>
+          <div id={styles.sale} className={styles.extraboldtext}>
+            SALE
+          </div>
+          <p id={styles.weekend} className={styles.boldtext}>
+            THIS WEEKEND ONLY
+          </p>
+          <p id={styles.upTo} className={styles.regulartext}>
+            Up to 50% OFF
+          </p>
+        </div>
+        <div className={styles.textcontent2}>
+        <p id={styles.specialoffer} className={styles.boldtext}>
+            SPECIAL <br/> OFFER!
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </>
     )
